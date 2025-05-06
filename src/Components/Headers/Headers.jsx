71dba20d72e46d2fa2311b2fad65f4daa39b07a3
dcartist <pbannerman13@gmail.com>
2025-05-06 19:05:20 -0400

    adding headers

---
+++ src/Components/Headers/Headers.jsx
@@ -0,0 +1,16 @@
+
+export const Header1 = ({headname}) => {
+    return (
+        <div className="header1">
+        <h1>{headname}</h1>
+        </div>
+    );
+    }
+
+export const Header2 = ({headname}) => {
+    return (
+        <div className="header2">
+        <h2>{headname}</h2>
+        </div>
+    );
+    }
