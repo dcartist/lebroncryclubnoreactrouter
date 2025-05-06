759638404f70cf6207cdc4ce2ef7f7806ca93c8e
dcartist <pbannerman13@gmail.com>
2025-05-06 19:26:05 -0400

    adding lebron

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import { Header1 } from './Components/Headers/Headers'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { MainPage } from './Sections/home/mainpage'
+import { Gallery } from "./Sections/Gallery/gallery"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <MainPage/>
+    <Gallery></Gallery>
     <Header1  headname="test"/>
      
     </>
--- src/Sections/home/mainpage.tsx
+++ src/Sections/home/mainpage.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './mainpage.css'
 
 export function MainPage() {
   return (
-    <div>
+    <div className="lebronCrySection1 anton-regular">
       <h1>Lebron Cry Club</h1>
     </div>
   )
